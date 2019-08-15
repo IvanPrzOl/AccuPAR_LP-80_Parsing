bd846ee879d67f211e1fc0921a9b0e8a2a9e7421
Ivan Perez <ivan.perezolivera@hotmail.com>
2019-08-15 09:52:06 -0500

    output dataframe as empty list

--- cept_proc.R
+++ cept_proc.R
@@ -27,61 +27,62 @@ data <- readWorksheet(wb,2) #Read daset from worksheet
 data <- subset(data,select = c("Record.Type","Date.and.Time","Annotation","Segment.1.PAR","Segment.2.PAR","Segment.3.PAR",
                                "Segment.4.PAR","Segment.5.PAR","Segment.6.PAR","Segment.7.PAR","Segment.8.PAR","Record.ID","Raw.Record.ID")) 
 
+rownames(data) <- as.character(as.numeric(rownames(data))+1)
+
 # Filter_by_dates and Trial ------
 annIndx <- data$Annotation[!is.na(data$Annotation)]
-grepl("SYN",data$Annotation,ignore.case = TRUE)
+annBoundaries <- c(0,which(!is.na(data$Annotation)))
+annBInFile <- data.frame(Annotation = annIndx,
+  initB = annBoundaries[1:(length(annBoundaries)-1)]+1,
+  finishB = annBoundaries[2:length(annBoundaries)])
 
+#unqDates <- unique((as.Date(data$Date.and.Time))) #year-month-day #Dates in file
 
-unqDates <- unique((as.Date(data$Date.and.Time))) #year-month-day #Dates in file
+toProcess <- annBInFile[ grepl("HIB",annBInFile$Annotation,ignore.case=TRUE), ]
 
-outDfList <- as.list(NULL)
-outIncDfList <-as.list(NULL)
+#outDfList <- as.list(NULL)
+#outIncDfList <-as.list(NULL)
 
-for(d in as.character(unqDates)){
+outputDf <- data.frame(NULL,stringsAsFactors = FALSE)
+  
+incAtn <- as.data.frame(NULL) #inconsistent annotations output dataframe
+
+for(k in 1:nrow(toProcess) ){
   #Output data frame structure
-  outputDf <- data.frame(OriginIdx = as.numeric(),
-                         Fecha = as.character(),
-                         Anotacion = as.numeric(),
-                         ARRIBA = as.numeric(),
-                         REFLEJADO = as.numeric(),
-                         ESPIGA = as.numeric(),
-                         HB = as.numeric(),
-                         H2 = as.numeric(),
-                         H3 = as.numeric(),
-                         ABAJO = as.numeric(), 
-                         stringsAsFactors = FALSE)
-  
-  incAtn <- as.data.frame(NULL) #inconsistent annotations output dataframe
 
-  subds <- (data %>% filter(as.Date(Date.and.Time) == d)) #all annotations in one day
-  row.names(subds) <- row.names(data[as.Date(data$Date.and.Time) == d,]) #keep origin Idx as row names
-  anB <- c(0,which(!is.na(subds$Annotation))) #Annotation boundaries
+
+  subds <- data[toProcess$initB[k]:toProcess$finishB[k],] #One annotation
+  row.names(subds) <- row.names(subds) #keep origin Idx as row names
   
-  for( k in 1:(length(anB)-1)){ #Process each annotation
-    reL <- AnnotationProc(subds[(anB[k]+1):anB[k+1],]) #Length >2 elements is ok
+  reL <- AnnotationProc(subds)
     
-    if(length(reL)>2){outputDf[nrow(outputDf)+1,] <- reL}
-    else{incAtn <- rbind(incAtn,reL)}
+  if(length(reL)>2){
+    outputDf <- rbind(outputDf,as.data.frame(reL))
   }
+  else{
+    incAtn <- rbind(incAtn,as.data.frame(reL))
+  }
+
   # Format data frame
-  outputDf <- outputDf %>% mutate(No.Plot = as.numeric(gsub("[A-Za-z\']","",outputDf$Anotacion)))
-  outputDf <- outputDf %>% arrange(No.Plot)
-  outputDf <- outputDf[,c(1:3,ncol(outputDf),4:(ncol(outputDf)-1))]
-  # print(outputDf)
+  #outputDf <- outputDf %>% mutate(No.Plot = as.numeric(gsub("[A-Za-z\']","",outputDf$Anotacion)))
+  #outputDf <- outputDf %>% arrange(No.Plot)
+  #outputDf <- outputDf[,c(1:3,ncol(outputDf),4:(ncol(outputDf)-1))]
+  
   
   # save_to_excel
-  owb <- loadWorkbook("C:/Users/IPOLIVERA/Documents/git_repos/Outputs/ceptometro_procesado.xlsx",create = TRUE)
-  createSheet(owb,name = d)
-  writeWorksheet(object = owb,data = outputDf, sheet = d)
-  saveWorkbook(owb)
-  
-  oiwb <- loadWorkbook("C:/Users/IPOLIVERA/Documents/git_repos/Outputs/ceptometro_no_procesado.xlsx",create = TRUE)
-  createSheet(oiwb,name = d)
-  writeWorksheet(object = oiwb,data = incAtn, sheet = d)
-  saveWorkbook(oiwb)
-  outDfList[[length(outDfList) + 1]] <- outputDf
-  outIncDfList[[length(outIncDfList) + 1]] <- incAtn
+  #owb <- loadWorkbook("C:/Users/IPOLIVERA/Documents/git_repos/Outputs/ceptometro_procesado.xlsx",create = TRUE)
+  #createSheet(owb,name = k)
+  #writeWorksheet(object = owb,data = outputDf, sheet = k)
+  #saveWorkbook(owb)
+  #
+  #oiwb <- loadWorkbook("C:/Users/IPOLIVERA/Documents/git_repos/Outputs/ceptometro_no_procesado.xlsx",create = TRUE)
+  #createSheet(oiwb,name = k)
+  #writeWorksheet(object = oiwb,data = incAtn, sheet = k)
+  #saveWorkbook(oiwb)
+  #outDfList[[length(outDfList) + 1]] <- outputDf
+  #outIncDfList[[length(outIncDfList) + 1]] <- incAtn
 #
   #print(outDfList)
   #print(outIncDfList)
 }
+print(outputDf)
