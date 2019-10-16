17aa2077b74d15d4e1feaae8dbb5f0c16b13765a
Iván Pérez <ivan.perezolivera@hotmail.com>
2019-10-16 10:36:08 -0500

    Merge branch, convert R script to R package
    * Added, How to install and use instructions
    
    * Added, examples images
    
    * Fixed some errors to build the package
    
    * Updated how to use
    
    Updated Radme.md and added new example images

--- R/cept_proc_funtions.R
+++ R/cept_proc_funtions.R
@@ -1,23 +1,23 @@
 #'Function to process one single annotation
+#' @importFrom stats sd median
 #'@param antn One Annotation Records
 #'@param nRecords Num of Records per annotation
 #'@param segments A vector indicating the PAR bar segments selected
 #'@param raw Raw outputdata
 #'@param parBarStats include some stastistics of the PAR bar in the output
-#'@return a dataframe with the means of each record or raw data if selected, if the annotation is not 
-#'consistent, returns null
-AnnProc <- function(antn,nRecords = 3,segments = 1:8,raw=FALSE,parBarStats=FALSE){ 
+#'@return a dataframe with the means of each record or raw data if selected, if the annotation is not
+AnnProc <- function(antn,nRecords = 3,segments = 1:8,raw=FALSE,parBarStats=FALSE){
   recordOrder <- list("3"=c("ABV","BLW","ABV"),
                       "7"=c("ABV","BLW","ABV","ABV","ABV","ABV","ABV"))
   labelMeans <- list("3" = c("ARRIBA","REFLEJADO","ABAJO"),
                       "7" = c("ARRIBA","REFLEJADO","ESPIGA","HB","H2","H3","ABAJO"))
   # Select PAR segments to process, this can be an optinal parameter
-  parSegments <- paste("Segement.",1:8,".PAR",sep="")[segments] 
+  parSegments <- paste("Segment.",1:8,".PAR",sep="")[segments]
 
   #Check consistency of the input data
   #Return a list with the origin index and the annotation name
   obs <- nrow(antn)
-  if((obs-1) < nRecords){ #missed data, 
+  if((obs-1) < nRecords){ #missed data,
     return(NULL)#list(originIdx = as.numeric(row.names(antn))[obs],Anotacion = antn$Annotation[obs]))
   }
   else if(any(!(antn$Record.Type[(obs-nRecords):(obs-1)] == recordOrder[[as.character(nRecords)]]))){ #Wrong record order
@@ -34,9 +34,9 @@ AnnProc <- function(antn,nRecords = 3,segments = 1:8,raw=FALSE,parBarStats=FALSE
   parMatrix = antn[(obs-nRecords):(obs-1),parSegments] #Numeric values of PAR segments
   parMeans = apply(as.matrix(parMatrix),1,mean)#rowMeans(parMatrix)
   names(parMeans) <- labelMeans[[as.character(nRecords)]] #Average in each strata
-  
+
   #Output list structure
-  out <- (c(list(originIndex = as.numeric(row.names(antn))[obs], 
+  out <- (c(list(originIndex = as.numeric(row.names(antn))[obs],
                 Fecha = trunc(antn$Date.and.Time[obs],"mins"),
                 Anotacion = antn$Annotation[obs],
                 Plot = as.numeric(gsub("\\D","",antn$Annotation[obs]))),parMeans))
@@ -46,15 +46,14 @@ AnnProc <- function(antn,nRecords = 3,segments = 1:8,raw=FALSE,parBarStats=FALSE
 
 #'Extract one annotation from dataframe given the annotation boundaries
 #'This fucntion calls directly the AnnProc function
-#'@param bdn A dataframe containing the name annotation names and their boundaries
+#'@param bnd A dataframe containing the name annotation names and their boundaries
 #'@param df ceptometer file dataframe
-#'@param antn One Annotation Records
 #'@param nRecords Num of Records per annotation
 #'@param segments A vector indicating the PAR bar segments selected
 #'@param raw Raw outputdata
 #'@param parBarStats include some stastistics of the PAR bar in the output
 #'@return a dataframe defined by AnnProc function
-SubsetByBnd <- function(bnd,df,nRecords,segments,raw,parBarStats){ 
+SubsetByBnd <- function(bnd,df,nRecords,segments,raw,parBarStats){
   return( as.data.frame( AnnProc(df[bnd[2]:bnd[3],],nRecords,segments,raw,parBarStats)) )
 }
 
@@ -68,21 +67,21 @@ SubsetByBnd <- function(bnd,df,nRecords,segments,raw,parBarStats){
 #'@param raw Raw outputdata
 #'@param parBarStats include some stastistics of the PAR bar in the output
 #'@return A data frame containing a subset of the annotations
-#'
+#'@export
 SubsetAnn <- function(df,tName,nRecords=3,segments=1:8,asDf=TRUE,raw=FALSE,parBarStats = FALSE){
-  # Get the annotation boundaries 
-  annBndIdx <- c(0,which(!is.na(df$Annotation))) 
+  # Get the annotation boundaries
+  annBndIdx <- c(0,which(!is.na(df$Annotation)))
   annBnd <- data.frame(Annotation = df$Annotation[!is.na(df$Annotation)],initB = annBndIdx[1:(length(annBndIdx)-1)]+1,finishB = annBndIdx[2:length(annBndIdx)])
 
 
   annBnd <- annBnd[ grepl(tName,annBnd$Annotation,ignore.case=TRUE), ]
-  
+
   if(asDf){ return (do.call('rbind',apply(annBnd,1,SubsetByBnd,df,nRecords,segments,raw,parBarStats))) }
 
-  else{ apply(annBnd,1,SubsetByBnd,df,nRecords,segments,raw,parBarStats) } 
+  else{ apply(annBnd,1,SubsetByBnd,df,nRecords,segments,raw,parBarStats) }
 }
 
-#'Calculate some stats of the PAR from the raw anotation 
+#'Calculate some stats of the PAR from the raw anotation
 #'@param parMat a Matrix of the PAR bar segments
 #'@param strat a vector with the name of each strata, i.e ARRIBA,REFLEC, etc.
 #'@return A list with the calculated params
@@ -93,7 +92,7 @@ getPARBarStats <- function(parMat,strat){
   names(parMin) <- paste("Min_",strat,sep="")
   parMax <- apply(parMat,1,max)
   names(parMax) <- paste("Max_",strat,sep="")
-  
+
   return(c(parSD,parMin,parMax))
 }
 
@@ -121,7 +120,7 @@ recordsOutlierReject <- function(records){
     bar <- as.numeric(records[k,paste("Segment.",1:8,".PAR",sep="")])
     limSup <- mean(bar) + 1*sd(bar)
     limInf <- mean(bar) - 1*sd(bar)
-    
+
   if(records[k,'Label'] == 'ABAJO'){
     bar[bar>limSup | bar<limInf] <-NA
     records[k,paste("Segment.",1:8,".PAR",sep="")] <- bar
--- readme.md
+++ readme.md
@@ -1,1 +1,55 @@
-R package to parse and order data from the AccuPAR LP-80 output files.
+# R package to parse and order data from the AccuPAR LP-80 output files.
+***
+
+## Instalación
+Antes de instalar este paquete hay que instalar la herramienta devtools ejecutanto el siguiente comando en R studio
+
+    install.library("devtools")
+
+Instalar el paquete para procesar datos del ceptómetro con:
+    
+    devtools::install_github("IvanPrzOl/AccuPAR_LP-80_Parsing")
+
+***
+## Descripción del archivo de salida
+El ceptómetro LP-80 puede exportar datos en diferentes formatos de salida como .csv, .txt o .xls  
+El formato .xls es un archivo de excel dos hojas:
+
+![Ejemplo de archivo](/captures/outputFile_Summary.PNG)
+
+![Ejemplo de archivo](/captures/outputFile_AllData.PNG)
+
+La hoja "Summary Data" contiene los promedios de cada anotación guardada en el ceptómetro.
+
+La hoja "All Data" contiene la información detalla de cada anotación que se guardó en el ceptómetro y es la hoja que nos interesa procesar.
+
+## Uso
+1. Cargar el paquete a la sesión de R con: 
+    
+        library(AccuPARParsing) 
+
+2. Importar la hoja "All Data" como un dataframe a R studio con el paquete readxl, xlsx o XLConnect.  
+    Ejemplo:
+        
+        library(xslx)
+        cept_data <- read.xlsx("cept_file.xls",sheetIndex = 2, header = TRUE) 
+        
+3. Llamar a la función SubsetAnn para procesar los datos del ceptometro indicando los siguientes parámetros.
+        
+        # df, dataframe importado del archivo excel.
+        # tName, Iniciales del ensayo que se desea extraer.
+        # nRecords, Número de muestras tomadas por cada anotación que corresponden a cada uno de los estratos medidos.
+        # segments, El número de los segmentos a extraer.
+        # raw, Indica si la salida de la función contiene los promedios de cada estrato o los datos crudos.
+        cept_procesado <- SubsetAnn( df = cept_data,
+                                     tName = "HIB", 
+                                     nRecords =3, 
+                                     segments = 1:8,
+                                     raw = TRUE)
+
+4. Dependiendo del valor pasado al parámetro raw (TRUE o FALSE), cept_procesado puede tener los siguientes formatos:
+
+### **raw=TRUE**
+![Salida en Raw](/captures/procRaw.PNG "asd")
+### **raw = FALSE**
+![Salida en means](/captures/procMeans.PNG)
