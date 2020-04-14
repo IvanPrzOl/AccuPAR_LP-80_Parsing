839025ff73682c1cb384e9e690d2a870cd94199b
Ivan Perez <ivan.perezolivera@hotmail.com>
2020-04-13 23:29:14 -0500

    change input parameters name

--- R/cept_proc_funtions.R
+++ R/cept_proc_funtions.R
@@ -1,36 +1,36 @@
-#'Function to process one single annotation
 #' @importFrom stats sd median
 
-ProcSingleAnn <- function(antn,segments = 1:8,raw=FALSE,parBarStats=FALSE,rOrder,trialName){
+#'Function to process one single annotation
+ProcSingleAnn <- function(singleAnotation,segments = 1:8,raw=FALSE,parBarStatistics=FALSE,recordOrder,trialName){
   #fill anotation cells
-  antn$Annotation <- antn$Annotation[!is.na(antn$Annotation)]
+  singleAnotation$Annotation <- singleAnotation$Annotation[!is.na(singleAnotation$Annotation)]
   #remove last row
-  antn <- antn[-nrow(antn),]
-  obs <- nrow(antn)
+  singleAnotation <- singleAnotation[-nrow(singleAnotation),]
+  obs <- nrow(singleAnotation)
   #delete non-used columns
-  antn <- antn[,-c(4:12,21:23)]
+  singleAnotation <- singleAnotation[,-c(4:12,21:23)]
   if (raw){
     if(!is.na(trialName)){
-            sep_annotation <- stringi::stri_match(antn$Annotation[obs],regex = sprintf('(%s)(\\d*)(.*)',trialName) )
+            sep_annotation <- stringi::stri_match(singleAnotation$Annotation[obs],regex = sprintf('(%s)(\\d*)(.*)',trialName) )
             sep_annotation[sep_annotation == ""] <- NA
-            antn$Trial.name <- sep_annotation[2] 
-            antn$Plot <- sep_annotation[3]
-            antn$Plot.obs <- sep_annotation[4]
+            singleAnotation$Trial.name <- sep_annotation[2] 
+            singleAnotation$Plot <- sep_annotation[3]
+            singleAnotation$Plot.obs <- sep_annotation[4]
     }
-    if (!is.na(rOrder)){
-      if(!(rOrder == paste(substr(antn$Record.Type,1,1) ,collapse = ''))){
-        print ( sprintf('plot %s cannot be extracted',antn$Annotation[obs]) )
-        print(paste(substr(antn$Record.Type,1,1) ,collapse = ''))
+    if (!is.na(recordOrder)){
+      if(!(recordOrder == paste(substr(singleAnotation$Record.Type,1,1) ,collapse = ''))){
+        print ( sprintf('plot %s cannot be extracted',singleAnotation$Annotation[obs]) )
+        print(paste(substr(singleAnotation$Record.Type,1,1) ,collapse = ''))
         return(NULL)
       }
     }
-    unique_records <- make.unique(antn$Record.Type,sep='')
+    unique_records <- make.unique(singleAnotation$Record.Type,sep='')
     record_order <- as.integer(gsub('\\D','',unique_records))
     record_order[is.na(record_order)] <- 0
     record_order <- record_order + 1
-    antn$Record.Type <- apply(cbind(seq(1:obs),antn$Record.Type,deparse.level = 0),1,function(z) paste(z[1],z[2],sep=''))
+    singleAnotation$Record.Type <- apply(cbind(seq(1:obs),singleAnotation$Record.Type,deparse.level = 0),1,function(z) paste(z[1],z[2],sep=''))
     
-    return(antn)
+    return(singleAnotation)
   
   }
   else{
@@ -41,30 +41,30 @@ ProcSingleAnn <- function(antn,segments = 1:8,raw=FALSE,parBarStats=FALSE,rOrder
 
 #'Process the ceptometer file as a dataframe
 #'
-#'@param df A dataframe readed from the ceptometer output file
-#'@param tName A string in regex format indicating the name of the annotation(s) to be extracted
-#'@param nRecords Num of Records per annotation
-#'@param segments A vector indicating the PAR bar segments selected
-#'@param raw Raw outputdata
-#'@param parBarStats include some stastistics of the PAR bar in the output
-#'@return A data frame containing a subset of the annotations
+#'@param ceptData A dataframe readed from the ceptometer output file
+#'@param trialName A string in regex format indicating the name of the annotation(s) to be extracted
+#'@param recordOrder A string of the expected record order, i.e 'ABA' means 'ABV','BLW','AVB'
+#'@param segments A vector indicating the PAR bar segments to be extracted
+#'@param raw A boolean to output data in raw format
+#'@param parBarStatistics A boolean, include some stastistics of the PARbar in the output
+#'@return A data frame containing all or a subset of the annotations
 #'@export
-CeptProc <- function(df,segments=1:8,raw=FALSE,parBarStats = FALSE,nRecords = NA ,tName = NA){
+CeptProc <- function(ceptData,segments=1:8,raw=FALSE,parBarStatistics = FALSE,recordOrder = NA ,trialName = NA){
   # Get the annotation boundaries
-  annBndIdx <- c(0,which(!is.na(df$Annotation)))
-  #annBnd <- data.frame(Annotation = df$Annotation[!is.na(df$Annotation)],initB = annBndIdx[1:(length(annBndIdx)-1)]+1,finishB = annBndIdx[2:length(annBndIdx)])
-  annBnd <- cbind(df$Annotation[!is.na(df$Annotation)], annBndIdx[1:(length(annBndIdx)-1)]+1, annBndIdx[2:length(annBndIdx)], deparse.level = 0)
+  annBndIdx <- c(0,which(!is.na(ceptData$Annotation)))
+  #annBnd <- data.frame(Annotation = ceptData$Annotation[!is.na(ceptData$Annotation)],initB = annBndIdx[1:(length(annBndIdx)-1)]+1,finishB = annBndIdx[2:length(annBndIdx)])
+  annBnd <- cbind(ceptData$Annotation[!is.na(ceptData$Annotation)], annBndIdx[1:(length(annBndIdx)-1)]+1, annBndIdx[2:length(annBndIdx)], deparse.level = 0)
 
-  if (!is.na(tName) && any(grepl(tName,annBnd[,1],ignore.case=TRUE))){
-    annBnd <- annBnd[ grepl(tName,annBnd[,1],ignore.case=TRUE), ]
+  if (!is.na(trialName) && any(grepl(trialName,annBnd[,1],ignore.case=TRUE))){
+    annBnd <- annBnd[ grepl(trialName,annBnd[,1],ignore.case=TRUE), ]
   }
   else{
     return(NULL)
   }
 
-  separatedAnn <- apply(annBnd,1,function(x,y) y[x[2]:x[3],],df)
+  separatedAnn <- apply(annBnd,1,function(x,y) y[x[2]:x[3],],ceptData)
 
-  return ( do.call('rbind',lapply(separatedAnn,ProcSingleAnn,segments,raw,parBarStats,nRecords,tName)) )
+  return ( do.call('rbind',lapply(separatedAnn,ProcSingleAnn,segments,raw,parBarStatistics,recordOrder,trialName)) )
 
 }
 
